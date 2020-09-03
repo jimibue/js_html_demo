d0e2541f0b320f11b1ac16f680e1ff76fdff4e3f
James Yeates <jimibue@gmail.com>
2020-09-03 11:33:39 -0700

    demo before lunch

--- index.js
+++ index.js
@@ -1,4 +1,48 @@
-console.log("Hello"); // you'll want to remove these in production
-// probably shouldn't be pushed to master on github
-let demo = "here";
-console.log(demo);
+// console.log("Hello"); // you'll want to remove these in production
+// // probably shouldn't be pushed to master on github
+// let demo = "here";
+// console.log(demo);
+// let myHeader = document.getElementById("header-1");
+
+// function handleChange() {
+//   console.log("clicked");
+//   myHeader.style.color = "Black";
+//   myHeader.innerHTML = "Header Changed";
+// }
+
+let person = { name: "Jonny", age: "33" };
+let person1 = { name: "Jimmy", age: "23" };
+let person2 = { name: "Jimmyx", age: "16" };
+
+let people = [person, person1, person2];
+
+// state - this responsible for keeping track of the data and current of app
+let state = {
+  pageHeader: "Contacts Page",
+  people: people,
+};
+
+// render method - takes state and renders it to html
+// note: so when ever state change we need to call render
+
+function renderPeople() {
+  peopleStr = "";
+  people.forEach(function (person) {
+    peopleStr += `<div>${person.name} is ${person.age}</div>`;
+  });
+  return peopleStr;
+}
+
+function render() {
+  htmlString = `<div>
+                  <h1>${state.pageHeader}</h1>
+                  ${renderPeople()}
+                </div>`;
+
+  renderPeople();
+
+  appElement = document.getElementById("app");
+  appElement.innerHTML = htmlString;
+}
+
+render();
